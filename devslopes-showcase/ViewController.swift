c875a5441a7270a6e883ed8bd1df1367d33c8e29
Tobias Gozzi <tobias.gozzi@me.com>
2016-10-06 18:09:04 +0200

    Initial Commit

---
+++ devslopes-showcase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  devslopes-showcase
+//
+//  Created by Tobias Gozzi on 06/10/2016.
+//  Copyright © 2016 Tobias Gozzi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
